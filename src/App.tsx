2ec4814ec70e8ba8b18556f39d0c9dfef85a0750
wellington <wellingtonslpbi@hotmail.com>
2020-10-23 08:53:50 -0300

    create logIn screen

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 import GlobalStyle from './styles/globals';
 
+import SignIn from './pages/SignIn';
+
 const App: React.FC = () => (
   <>
-    <h1>Hello World</h1>
+    <SignIn />
     <GlobalStyle />
   </>
 );
